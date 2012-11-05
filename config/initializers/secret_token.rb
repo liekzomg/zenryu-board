38c5b8b1f9090a6be7d1c09ac16bba3e74136b47
Ganryu <spammage@gmx.com>
2012-11-05 23:40:32 +0800

    $ rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZenryuBoard::Application.config.secret_token = '77cd1ed237218e1088a39df1374b2c6db7e31835e2e1409b41bfdefd13c32b693d88f753134a26cb0fd884f79b39be1065fb8ae9b8ad576ab61711ae63121e89'
